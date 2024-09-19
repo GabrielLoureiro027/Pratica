90aa8dc7cc76e62764c763da30a37b207ee42aa9
Gabriel Loureiro <loureirogabriel672@gmail.com>
2024-09-19 11:20:28 -0300

    adfadf

---
+++ 3 - Manipulação de Notas de Alunos/js.js
@@ -0,0 +1,41 @@
+// TODO : Definindo as principais variaveis e classes
+class ArquivoOFC{
+        constructor(nome, nota1 = "", nota2 = ""){
+            this.nome = nome;
+            this.nota1 = nota1;
+            this.nota2 = nota2;
+        }       
+    }
+
+
+const alunos = [
+    nome = "",
+    notas = parseFloat(""),
+    media = "",
+]
+const qntAprovados = [];
+// TODO : Ler o arquivo e criar um array de objetos com as informações dos alunos
+//Funções ler Arquivo
+lerArquivoOFC(){
+    const Notas = this.nota1.split('\n').map(row) => row.split(",")
+    const headers = csvArray.shift();   
+}
+// Funções
+calcularMedia(alunos);
+alunosAprovados(alunos);
+
+    function calcularMedia(alunos){
+        alunos.notas.forEach(nota => {
+            media = (nota[0] + nota[1])/2
+            console.log("Media foi")
+        });
+        // TODO : listando os alunos
+        alunos.forEach(alunos =>  {
+            if(alunos >= alunos.length){
+                return (`Nome${alunno.nome}: <br>${media}`)
+            }  
+        });
+    }
+    function alunosAprovados(){
+
+    }
