274c682064ff0a14ec93296685448c80c5db3623
Gabriel Loureiro <loureirogabriel672@gmail.com>
2024-10-29 11:16:02 -0300

    Preparação
    Preparação para o script de exibir imagens do pinterest

---
+++ ProjetoUm/src/ExibirFotos.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const photos = [
+    { id: 1, title: 'Foto 1', url: 'url-da-foto-1.jpg' },
+    { id: 2, title: 'Foto 2', url: 'url-da-foto-2.jpg' },
+  ];
+
+export default function ExibirFotos() {
+  return 
+  const photos = [
+  { id: 1, title: 'Foto 1' },
+  { id: 2, title: 'Foto 2' },
+];
+  <div>
+    
+  </div>;
+}
--- ProjetoUm/src/Pages/Home/index.jsx
+++ ProjetoUm/src/Pages/Home/index.jsx
@@ -8,17 +8,18 @@ export default function Home() {
         Bem vindo ao <span className="">ConnectEvents</span>
       </h1>
       <p className="my-3">
-        Na ConnectEvents, acreditamos que cada
-        evento é uma oportunidade única de criar conexões significativas. Com
-        uma abordagem personalizada e inovadora, nos dedicamos a transformar
-        suas ideias em experiências memoráveis. Seja uma festa, uma conferência
-        ou um evento corporativo, nossa equipe de especialistas está pronta para
-        planejar e executar cada detalhe, garantindo que tudo aconteça de forma
-        impecável. Junte-se a nós e descubra como podemos tornar seu próximo
-        evento extraordinário!
+        Na ConnectEvents, acreditamos que cada evento é uma oportunidade única
+        de criar conexões significativas. Com uma abordagem personalizada e
+        inovadora, nos dedicamos a transformar suas ideias em experiências
+        memoráveis. Seja uma festa, uma conferência ou um evento corporativo,
+        nossa equipe de especialistas está pronta para planejar e executar cada
+        detalhe, garantindo que tudo aconteça de forma impecável. Junte-se a nós
+        e descubra como podemos tornar seu próximo evento extraordinário!
       </p>
       <section id="areaEventos" className="font-semibold">
-      <h1>Eventos</h1>
+        <h1>Eventos</h1>
+        <div id="exibirFotos">
+        </div>
       </section>
     </div>
   );
