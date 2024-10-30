71eef7e55ba38cff82c0b3d5db5f4254b22c2c72
Gabriel Loureiro <loureirogabriel672@gmail.com>
2024-10-30 10:34:41 -0300

    Tentando exibir fotos
    Excluindo e editando array para tentar exibir as fotos

---
+++ ProjetoUm/src/Fotos/DetalhesFotos.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function DetalhesFotos() {
+  return (
+    <>
+     <h1>Tittle</h1>
+     <img src="{foto.url}" alt="" />
+    </>
+  )
+}
---
+++ ProjetoUm/src/Fotos/InfoFotos.js
@@ -0,0 +1,7 @@
+const fotos = [
+  { id: 1, title: 'Foto 1', url: '/foto-1.jpg', descricao:'imagem 1' },
+  { id: 1, title: 'Foto 2', url: '/foto-2.jpg', descricao:'imagem 2' },
+  { id: 1, title: 'Foto 3', url: '/foto-3.jpg', descricao:'imagem 3' },
+  { id: 1, title: 'Foto 4', url: '/foto-4.jpg', descricao:'imagem 4' },
+  { id: 1, title: 'Foto 5', url: '/foto-5.jpg', descricao:'imagem 5' },
+];
